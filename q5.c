bdb3d554fbc118eb081e97dcf61a2b800d167d45
jainjai266 <jainjai266@gmail.com>
2026-08-10 22:06:14 +0530

    Add files via upload

---
+++ q5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+printf("value of a");
+scanf("%d",&a);
+printf("value of a is %d",a);
+int b;
+printf("value of b");
+scanf("%d",&b);
+printf("value of b is %d\n",b);
+int sum =  a+b;
+printf("sum is %d\n",sum);
+return 0;
+}
