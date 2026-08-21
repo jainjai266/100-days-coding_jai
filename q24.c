aed06ecc3b6cad7327cd74728f504ae9a56a7547
jainjai266 <jainjai266@gmail.com>
2026-08-21 18:32:25 +0530

    Add files via upload

---
+++ q24.c
@@ -0,0 +1,34 @@
+/*Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit
+*/
+#include<stdio.h>
+int main()
+{
+    int unit,bill;
+    printf("enter the unit consumed\n");
+    scanf("%d",&unit);
+    if(unit<=100)
+    {
+        bill=unit*5;
+        printf("bill is %d",bill);
+    }
+    else if(unit<=200&&unit>100)
+    {
+    bill=(100*5)+((unit-100)*7);
+    printf("bill is %d",bill);
+    }
+    else if(unit<=300&&unit>200)
+    {
+    bill=(100*5)+(100*7)+((unit-200)*10);
+    printf("bill is %d",bill);
+    }
+     else
+    {
+        bill=(100*5)+(100*7)+(100*10)+((unit-300)*12);
+        printf("bill is %d",bill);
+    }
+    return 0;
+}
