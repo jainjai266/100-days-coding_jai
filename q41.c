e059eb1889eb89b557ba47d32e27982abd29aef2
jainjai266 <jainjai266@gmail.com>
2026-08-30 23:46:24 +0530

    Add files via upload

---
+++ q41.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, first, last, temp, power = 1, result;
+
+    scanf("%d", &n);
+
+    last = n % 10;
+    temp = n;
+
+    while (temp >= 10) {
+        temp = temp / 10;
+        power = power * 10;
+    }
+
+    first = temp;
+
+    result = n - first * power - last;
+    result = result + last * power + first;
+
+    printf("%d", result);
+
+    return 0;
+}
