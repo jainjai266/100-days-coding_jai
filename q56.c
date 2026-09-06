74215a09985ee31376dd2255b868553f4f1a5c47
jainjai266 <jainjai266@gmail.com>
2026-09-06 23:16:40 +0530

    Add files via upload

---
+++ q56.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int arr[100];
+
+    scanf("%d", &n);
+
+    // Read array elements
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print array elements
+    for(i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+
+        if(i < n - 1)
+            printf(" ");
+    }
+
+    return 0;
+}
