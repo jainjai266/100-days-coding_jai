555dcf592763118f393f5e25607a389fd33001cb
jainjai266 <jainjai266@gmail.com>
2026-09-12 22:08:22 +0530

    Add files via upload

---
+++ q67.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a[100], pos, element;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d %d", &pos, &element);
+
+    for(int i = n; i > pos; i--)
+    {
+        a[i] = a[i - 1];
+    }
+
+    a[pos] = element;
+    n++;
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
